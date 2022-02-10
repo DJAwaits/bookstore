5b21c6e08f6f4577076c35bdb6a3c538786207d1
DJAwaits <padjcreeper@gmail.com>
2022-02-10 12:46:27 -0800

    3rd commit book project slide 36

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -12,14 +12,51 @@ module.exports.viewProfile = async function(req, res){
     res.render('books/profile', {book})
 };
 //render add form
-
+module.exports.renderAddForm = function(req, res){
+    const book = {
+        title: '',
+        author: '',
+        publisher: '',
+        genre: genres[0],
+        pageCount: '',
+        cover: '',
+        description: ''
+    };
+    res.render('books/add', {book, genres});
+};
 //add
-
+module.exports.addBook = async function(req, res){
+    const book = await Book.create({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pageCount: req.body.pageCount,
+        cover: req.body.cover,
+        description: req.body.description
+    });
+    res.redirect(`/books/profile/${book.id}`);
+};
 //render edit form
 module.exports.renderEditForm = async function(req, res){
     const book = await Book.findByPk(req.params.id);
     res.render('books/edit', {book, genres});
 };
 //update
-
+module.exports.updateBook = async function(req, res){
+    const book = await Book.update({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pageCount: req.body.pageCount,
+        cover: req.body.cover,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+        });
+    res.redirect(`/courses/profile/${req.params.id}`);
+};
 //delete
